1d3c2c48d9cf8944c45158716d819ec7025c3929
ivarnos <ivarnos@student.hist.no>
2016-06-09 17:11:51 +0200

    Feed, closing polish Chat, closing rewamp

--- src/main/java/com/basiv/server/App/MyApp.java
+++ src/main/java/com/basiv/server/App/MyApp.java
@@ -2,7 +2,6 @@ package com.basiv.server.App;
 
 import com.basiv.server.MessageBodyWriters.ArrayListMessageBodyWriter;
 import com.basiv.server.MessageBodyWriters.StringListMessageBodyWriter;
-import com.basiv.server.Models.ScoreEntity;
 import com.basiv.server.Resources.AuthResource;
 import com.basiv.server.Resources.CategoryResource;
 import com.basiv.server.Resources.HashtagResource;
@@ -11,21 +10,11 @@ import com.basiv.server.Resources.MatchResource;
 import com.basiv.server.Resources.ProfileResource;
 import com.basiv.server.Resources.TeamResource;
 import com.basiv.server.Resources.UserResource;
-import com.basiv.server.Services.MatchService;
-import com.basiv.server.config.MongoDB;
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientOptions;
-import com.mongodb.ServerAddress;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
-import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.Application;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
-import org.mongodb.morphia.Datastore;
-import org.mongodb.morphia.Morphia;
 
 
 /**
